ae63e2e2235999c395155357efad95e07c6f0802
Yitzkak <bevinchi@gmail.com>
2024-01-10 14:45:05 +0100

    using whisper locall instead of using API

--- transcription.py
+++ transcription.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import openai
+import whisper
 
 API_KEY = "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 "
 model_id = "whisper-1"
 
-audio = st.file_uploader("Upload a file")
+audio_file = st.file_uploader("Upload a file")
 
-if audio is not None:
-    audio_file_path = audio.name 
-    audio_file = open(audio_file_path, "rb")
-    transcript = openai.Audio.transcribe(model_id, audio_file, API_KEY)
-    st.write(transcript)
+model = whisper.load_model("base")
+
+if audio_file is not None:
+    result = model.transcribe(audio_file.name)
+    st.write(result["text"])
+    st.write(audio_file.name)
